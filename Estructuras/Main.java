0198a98b3b5b14608801d204adf90cbfd8c1e1e4
cesar.angel <cesar.cfeteit@gmail.com>
2023-02-02 13:10:33 -0600

    Indicaciones replica

--- Estructuras/Main.java
+++ Estructuras/Main.java
@@ -1,20 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Estructura de control FOR:");
-        printNumbersUsingFor();
-        System.out.println("\nEstructura de control WHILE:");
-        printNumbersUsingWhile();
-        System.out.println("\nEstructura de control DO WHILE:");
-        printNumbersUsingDoWhile();
+        System.out.println("Estructura de control For:");
+        ImprimirnumerosFor();
+        System.out.println("\nEstructura de control While:");
+        ImprimirnumerosWhile();
+        System.out.println("\nEstructura de control Do While:");
+        ImprimirnumerosdoWhile();
     }
 
-    public static void printNumbersUsingFor() {
+    public static void ImprimirnumerosFor() {
         for (int i = 2; i <= 100; i += 2) {
             System.out.print(i + "-");
         }
     }
 
-    public static void printNumbersUsingWhile() {
+    public static void ImprimirnumerosWhile() {
         int i = 2;
         while (i <= 100) {
             System.out.print(i + "-");
@@ -22,7 +22,7 @@ public class Main {
         }
     }
 
-    public static void printNumbersUsingDoWhile() {
+    public static void ImprimirnumerosdoWhile() {
         int i = 2;
         do {
             System.out.print(i + "-");
