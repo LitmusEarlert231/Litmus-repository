1d946e829d9b06500a343e4bfb735162ec23a580
cesar.angel <cesar.cfeteit@gmail.com>
2023-02-13 14:54:56 -0600

    ls

---
+++ EjercicioTaller - copia/src/ejerciciotaller/Gestion.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciotaller;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author CGarcia
+ */
+public class Gestion {
+    public void menu(){
+    //Registro de clientes
+    //Lista clientes
+    ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+    
+    //opciones menu
+    }
+    
+    //Agregue clientes en la lista
+    public void Agregar_cliente(ArrayList<Cliente> clientes){
+    }
+}
